33af032f630614fdd34b7203e3e60c5f5845821f
avif444 <avifmuhamadtaufiq4@gmail.com>
2018-02-03 06:00:19 +0700

    feat(gegeg): geg
    ge

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,9 @@
 var expect = require('chai').expect;
 
 describe('test1', function() {
-  it('should works', function() {
-    expect(true).to.be.true;
+  describe('all', function() {
+    it('should works', function() {
+      expect(true).to.be.true;
+    });
   });
 });
